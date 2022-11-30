ec8daa42af9f9ebf6c75671fbf01b9b10244db89
ricardoxps <ricardoxp@gmail.com>
2022-11-30 14:29:49 -0300

    Formulario cadasto

--- JavaScript1.js
+++
@@ -1,9 +0,0 @@
-function Enviar() {
-
-    var nome = document.getElementById("nomeid");
-
-    if (nome.value != "") {
-        alert('Obrigado: ' + nome.value + ' Agradecemos seu contato.');
-    }
-
-}
